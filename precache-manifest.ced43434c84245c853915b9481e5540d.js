f083139db953d4737ad431c04064561f13220ac3
Jurian <j.vink@gdksoftware.nl>
2019-12-18 00:17:01 +0100

    Updates

---
+++ precache-manifest.ced43434c84245c853915b9481e5540d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0b9cfac364fbf4035c6fb60416651a",
+    "url": "/solar-system/index.html"
+  },
+  {
+    "revision": "7789f0f1d6713092d2ad",
+    "url": "/solar-system/static/css/main.6e6f6b4f.chunk.css"
+  },
+  {
+    "revision": "e18125d1b8d82f4372d1",
+    "url": "/solar-system/static/js/2.53a95069.chunk.js"
+  },
+  {
+    "revision": "7de9d750d1785587126615108e0498b6",
+    "url": "/solar-system/static/js/2.53a95069.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7789f0f1d6713092d2ad",
+    "url": "/solar-system/static/js/main.b4ea3f5a.chunk.js"
+  },
+  {
+    "revision": "53fc7aaff3a546ed6f44",
+    "url": "/solar-system/static/js/runtime-main.27e92ecf.js"
+  }
+]);
